8944748ad1b62a84476c5e4e6cfbeb7cf8d4f421
Lalita123456 <6431503051@lamduan.mfu.ac.th>
2024-02-05 14:02:28 +0700

    commit

---
+++ pages/inspire.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220; THANK YOU &#8221;
+        <footer>
+          <small>
+            <em>&mdash;LILY MEKKALAR</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage',
+}
+</script>
